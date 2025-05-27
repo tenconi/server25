402e95688a43c36b8464887d101fffaf7a06e998
tenconi <47565305+tenconi@users.noreply.github.com>
2025-05-28 01:27:43 +0200

    First Uploaded: adding details

---
+++ backend/src/persistence/mongoDB/db.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+import config from '../../config.js';
+
+try {
+  await mongoose.connect(config.MONGO_URL);
+  console.log('♠ Conectado a MONGO');
+} catch (error) {
+  console.log('Error al conectar a MONGO:', error);
+}
