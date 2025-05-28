37f3a1d943be3fe08ce68894e9e90899661c28cc
tenconi <47565305+tenconi@users.noreply.github.com>
2025-05-28 11:12:22 +0200

    Starting with Routes

---
+++ backend/src/routes/products.router.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+// import Class from Controllers
+
+const router = Router();
+
+router.post('/add', importClassFromController)
+
+router.get('/list', importClassFromController)
+
+
+
+
+
+export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,25 @@
 import express from 'express';
+import { __dirname } from './utils.js';
+import productsRouter from './routes/products.router.js';
+import usersRouter from './routes/users.router.js';
 
 const app = express();
 
+
+// server incoming requests
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// middleware "static" + dirname
+app.use(express.static(__dirname + '/public'));
+
+
+// routes:
+app.use('/products', productsRouter)
+app.use('/users', usersRouter)
+
+
+
+// server
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
